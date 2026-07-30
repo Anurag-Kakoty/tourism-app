f46825777d279010436ecd3cfc81e95eb72971d2
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-07-30 22:31:44 +0530

    language enum for guide

---
+++ backend/src/main/java/com/tourism/backend/guide/entity/Language.java
@@ -0,0 +1,26 @@
+package com.tourism.backend.guide.entity;
+
+public enum Language {
+
+    ENGLISH,
+    HINDI,
+    ASSAMESE,
+    KHASI,
+    GARO,
+    BENGALI,
+    TAMIL,
+    TELUGU,
+    KANNADA,
+    MALAYALAM,
+    MARATHI,
+    GUJARATI,
+    PUNJABI,
+    ODIA,
+    NEPALI,
+    SANSKRIT,
+    FRENCH,
+    GERMAN,
+    SPANISH,
+    JAPANESE,
+    CHINESE
+}
