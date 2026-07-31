f9c5766fedb7cd15f901b260ef7d8d10ee45cbae
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-07-31 21:08:48 +0530

    enum for type of food/cuisine

---
+++ backend/src/main/java/com/tourism/backend/restaurant/entity/Cuisine.java
@@ -0,0 +1,17 @@
+package com.tourism.backend.restaurant.entity;
+
+public enum Cuisine {
+
+    ASSAMESE,
+    MEGHALAYAN,
+    NORTH_INDIAN,
+    SOUTH_INDIAN,
+    CHINESE,
+    CONTINENTAL,
+    ITALIAN,
+    MEXICAN,
+    CAFE,
+    FAST_FOOD,
+    SEAFOOD,
+    MULTI_CUISINE
+}
