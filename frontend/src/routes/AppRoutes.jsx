67b66ebd6da5b18780af187240671a96f485e504
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-03 19:41:33 +0530

    created app routes

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home/Home";
+import Places from "../pages/Places/Places";
+import PlaceDetails from "../pages/Places/PlaceDetails";
+import Experiences from "../pages/Experiences/Experiences";
+import Festivals from "../pages/Festivals/Festivals";
+import Stay from "../pages/Stay/Stay";
+import Guides from "../pages/Guides/Guides";
+import Transport from "../pages/Transport/Transport";
+import Itinerary from "../pages/Itinerary/Itinerary";
+import NotFound from "../pages/NotFound/NotFound";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/places" element={<Places />} />
+      <Route path="/places/:id" element={<PlaceDetails />} />
+      <Route path="/experiences" element={<Experiences />} />
+      <Route path="/festivals" element={<Festivals />} />
+      <Route path="/stay" element={<Stay />} />
+      <Route path="/guides" element={<Guides />} />
+      <Route path="/transport" element={<Transport />} />
+      <Route path="/itinerary" element={<Itinerary />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
