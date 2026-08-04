72817e6bc01a468bf2cd91bee84cc82da510eec7
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-04 21:45:15 +0530

    constants for navigation

---
+++ frontend/src/constants/navigation.js
@@ -0,0 +1,32 @@
+const navigation = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Places",
+    path: "/places",
+  },
+  {
+    label: "Experiences",
+    path: "/experiences",
+  },
+  {
+    label: "Festivals",
+    path: "/festivals",
+  },
+  {
+    label: "Stay",
+    path: "/stay",
+  },
+  {
+    label: "Guides",
+    path: "/guides",
+  },
+  {
+    label: "Transport",
+    path: "/transport",
+  },
+];
+
+export default navigation;
