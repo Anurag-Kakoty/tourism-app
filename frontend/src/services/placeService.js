0d2c0817f7e6c31415d0ebe95db19fd6990302c2
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-07 22:29:33 +0530

    using the api file from the constants

--- frontend/src/services/placeService.js
+++ frontend/src/services/placeService.js
@@ -1,13 +1,14 @@
 import api from "./api";
+import API from "../constants/api";
 
 const placeService = {
   async getAll() {
-    const response = await api.get("/attractions");
+    const response = await api.get(API.ATTRACTIONS);
     return response.data;
   },
 
   async getById(id) {
-    const response = await api.get(`/attractions/${id}`);
+    const response = await api.get(`${API.ATTRACTIONS}/${id}`);
     return response.data;
   },
 };
