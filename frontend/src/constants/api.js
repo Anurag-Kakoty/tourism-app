2e890d740c2037bd1ea89d75dca21d95572b4fb4
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-07 22:28:21 +0530

    file to store apis

---
+++ frontend/src/constants/api.js
@@ -0,0 +1,13 @@
+const API = {
+  ATTRACTIONS: "/attractions",
+  DESTINATIONS: "/destinations",
+  STATES: "/states",
+  FESTIVALS: "/festivals",
+  EXPERIENCES: "/experiences",
+  ACCOMMODATIONS: "/accommodations",
+  GUIDES: "/guides",
+  TRANSPORTS: "/transports",
+  ITINERARIES: "/itineraries",
+};
+
+export default API;
