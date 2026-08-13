cd0a01c615dc0018f417de6a3783793661f7ac45
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-13 23:24:55 +0530

    transport service

---
+++ frontend/src/services/transportService.js
@@ -0,0 +1,58 @@
+import api from "./api";
+import API from "../constants/api";
+
+const transportService = {
+  async getAll() {
+    const response = await api.get(API.TRANSPORT);
+    return response.data;
+  },
+
+  async getById(id) {
+    const response = await api.get(
+      `${API.TRANSPORT}/${id}`
+    );
+
+    return response.data;
+  },
+
+  async getByDestination(destinationId) {
+    const response = await api.get(
+      API.TRANSPORT,
+      {
+        params: {
+          destinationId,
+        },
+      }
+    );
+
+    return response.data;
+  },
+
+  async getByType(type) {
+    const response = await api.get(
+      API.TRANSPORT,
+      {
+        params: {
+          type,
+        },
+      }
+    );
+
+    return response.data;
+  },
+
+  async getByAvailability(available = true) {
+    const response = await api.get(
+      API.TRANSPORT,
+      {
+        params: {
+          available,
+        },
+      }
+    );
+
+    return response.data;
+  },
+};
+
+export default transportService;
