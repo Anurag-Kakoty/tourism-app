9ed2a4f364524dcf8c595dc69163b4537942150f
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-08-19 11:17:54 +0530

    handle dynamic filters

--- frontend/src/services/destinationService.js
+++ frontend/src/services/destinationService.js
@@ -2,24 +2,37 @@ import api from "./api";
 import API from "../constants/api";
 
 const destinationService = {
-  async getAll() {
-    const response = await api.get(API.DESTINATIONS);
-    return response.data;
-  },
-
-  async getById(id) {
-    const response = await api.get(`${API.DESTINATIONS}/${id}`);
-    return response.data;
-  },
-
-  async getByState(stateId) {
+  async getAll(filters = {}) {
     const response = await api.get(API.DESTINATIONS, {
       params: {
-        stateId,
+        stateId: filters.stateId || undefined,
+        type: filters.type || undefined,
+        featured:
+          filters.featured !== "" && filters.featured !== undefined
+            ? filters.featured
+            : undefined,
+        popular:
+          filters.popular !== "" && filters.popular !== undefined
+            ? filters.popular
+            : undefined,
       },
     });
 
     return response.data;
+  },
+
+  async getById(id) {
+    const response = await api.get(
+      `${API.DESTINATIONS}/${id}`
+    );
+
+    return response.data;
+  },
+
+  async getByState(stateId) {
+    return this.getAll({
+      stateId,
+    });
   },
 };
 
