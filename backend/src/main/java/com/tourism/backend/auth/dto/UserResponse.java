8a53404b6cfa99133c79f49057ce3137fd875ef5
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-09-10 11:55:51 +0530

    user response

---
+++ backend/src/main/java/com/tourism/backend/auth/dto/UserResponse.java
@@ -0,0 +1,17 @@
+package com.tourism.backend.auth.dto;
+
+import com.tourism.backend.user.entity.Role;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+}
