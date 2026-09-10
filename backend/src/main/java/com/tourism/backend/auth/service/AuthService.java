755dec6a74381f57c1bc8ec5827e40fc85db86c5
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-09-10 17:50:48 +0530

    use login request and response dto

--- backend/src/main/java/com/tourism/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/tourism/backend/auth/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.tourism.backend.auth.service;
 
+import com.tourism.backend.auth.dto.LoginRequest;
+import com.tourism.backend.auth.dto.LoginResponse;
 import com.tourism.backend.auth.dto.RegisterRequest;
 import com.tourism.backend.auth.dto.UserResponse;
 
 public interface AuthService {
 
     UserResponse register(RegisterRequest request);
+
+    LoginResponse login(LoginRequest request);
 }
