b829a6d4f1fd47068094fcb0b84e0b0b8717d46b
Anurag-Kakoty <anuragkakoty@gmail.com>
2026-09-10 11:55:41 +0530

    user repository

---
+++ backend/src/main/java/com/tourism/backend/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tourism.backend.user.repository;
+
+import com.tourism.backend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
+}
